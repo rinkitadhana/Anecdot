4988ce012c295dca5074091512354b542b726ec6
Rinkit Adhana <therinkit@gmail.com>
2024-08-10 01:27:46 +0530

    add

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,8 +1,9 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
-const SignUp = () => {
+export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [errorMessage, setErrorMessage] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -36,56 +37,71 @@ const SignUp = () => {
       setLoading(false);
     }
   };
-
   return (
-    <div className=" mt-20 ">
-      <div className=" p-4 md:p-3 max-w-sm mx-auto md:items-center">
-        <div className="">
-          <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left */}
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Sahand's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            This is a demo project. You can sign up with your email and password
+            or with Google.
+          </p>
+        </div>
+        {/* right */}
+
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
-              <Label value="Username" />
+              <Label value="Your username" />
               <TextInput
                 type="text"
-                placeholder="username"
+                placeholder="Username"
                 id="username"
                 onChange={handleChange}
               />
             </div>
             <div>
-              <Label value="Email" />
+              <Label value="Your email" />
               <TextInput
                 type="email"
-                placeholder="email"
+                placeholder="name@company.com"
                 id="email"
                 onChange={handleChange}
               />
             </div>
             <div>
-              <Label value="Password" />
+              <Label value="Your password" />
               <TextInput
                 type="password"
-                placeholder="password"
+                placeholder="Password"
                 id="password"
                 onChange={handleChange}
               />
             </div>
             <Button
-              className=" mt-5"
-              gradientDuoTone="purpleToBlue"
+              gradientDuoTone="purpleToPink"
               type="submit"
               disabled={loading}
             >
               {loading ? (
                 <>
-                  <Spinner size="sm" /> <span className="pl-3">Loading...</span>{" "}
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
                 </>
               ) : (
                 "Sign Up"
               )}
             </Button>
+            <OAuth />
           </form>
-          <div className="flex gap-2 text-sm mt-5 justify-center">
-            <span>Already have an account?</span>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
             <Link to="/sign-in" className="text-blue-500">
               Sign In
             </Link>
@@ -99,6 +115,4 @@ const SignUp = () => {
       </div>
     </div>
   );
-};
-
-export default SignUp;
+}
