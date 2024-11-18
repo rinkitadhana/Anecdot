4be8f9e917e5a32d52ac7625fd3aa096307bdf83
Rinkit Adhana <therinkit@gmail.com>
2024-11-19 00:43:39 +0530

    footer-code-inhance

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,22 +4,26 @@ import { FiGithub, FiInstagram, FiLinkedin } from "react-icons/fi"
 const Footer = () => {
   const links = [
     {
+      id: 1,
       name: "Github",
       link: "https://github.com/rinkitadhana",
       icon: <FiGithub />,
     },
     {
+      id: 2,
       name: "Twitter",
       link: "https://twitter.com/damnGruz",
       icon: <FaXTwitter />,
     },
     {
+      id: 3,
       name: "LinkedIn",
       link: "https://www.linkedin.com/in/rinkitadhana",
       icon: <FiLinkedin />,
     },
 
     {
+      id: 4,
       name: "Instagram",
       link: "https://www.instagram.com/rnkktt",
       icon: <FiInstagram />,
@@ -27,35 +31,20 @@ const Footer = () => {
   ]
   return (
     <footer className="flex flex-col gap-1 items-center justify-centers py-8">
-      <div className="hidden md:flex flex-wrap gap-2 md:gap-4 items-center">
+      <div className="flex flex-wrap gap-5 md:gap-4 items-center">
         {links.map((link) => (
-          <>
-            <a
-              href={link.link}
-              key={link.name}
-              target="_blank"
-              className=" font-medium text-zinc-800 hover:text-zinc-500 flex flex-row gap-0.5 items-center"
-            >
-              {link.icon}
-              {link.name}
-            </a>
-          </>
+          <a
+            href={link.link}
+            key={link.id}
+            target="_blank"
+            className=" font-medium text-xl md:text-base text-zinc-800 md:hover:text-zinc-500 flex flex-row gap-0.5 items-center"
+          >
+            {link.icon}
+            <span className=" hidden md:block"> {link.name}</span>
+          </a>
         ))}
       </div>
-      <div className="md:hidden flex flex-wrap gap-5 md:gap-4 items-center">
-        {links.map((link) => (
-          <>
-            <a
-              href={link.link}
-              key={link.name}
-              target="_blank"
-              className=" text-xl font-medium text-zinc-800 hover:text-zinc-500 flex flex-row gap-0.5 items-center"
-            >
-              {link.icon}
-            </a>
-          </>
-        ))}
-      </div>
+
       <div className=" text-black/95">
         © 2024 Rinkit Adhana. All rights reserved.
       </div>
