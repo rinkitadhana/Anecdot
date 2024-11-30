c39cd6c58f0d2fc2cebc52ac43d523d9f03a2125
Rinkit Adhana <therinkit@gmail.com>
2024-11-30 18:04:04 +0530

    about-update

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,11 +1,17 @@
 import Back from "../components/Back"
 import { IoPlay } from "react-icons/io5"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { FiGithub } from "react-icons/fi"
+import { FaInstagram, FaXTwitter } from "react-icons/fa6"
+import { FiLinkedin } from "react-icons/fi"
+import { LuGithub } from "react-icons/lu"
+import { SiLeetcode } from "react-icons/si"
 
 const About = () => {
-  const [visClient, setVisClient] = useState(false)
-  const [visServer, setVisServer] = useState(false)
+  const [visClient, setVisClient] = useState(true)
+  const [visServer, setVisServer] = useState(true)
+  const description =
+    "A full stack blogging website built using the MERN stack, offering avariety of features to enhance user experience. The website allows users to manage their profiles, and engage through comments and likes. It also includes user authentication and real-time updates."
   const client = [
     {
       name: "React JS",
@@ -33,7 +39,7 @@ const About = () => {
   ]
   const feat = [
     {
-      name: "Easy to use UI and fun Project",
+      name: "Easy to use UI and fun Project Easy to use UI and fun Project Easy to use UI and fun Project Easy to use UI and fun Project Easy to use UI and fun Project Easy to use UI and fun Project Easy to use UI and fun Project",
     },
     {
       name: "Easy to use UI and fun Projectt",
@@ -45,21 +51,35 @@ const About = () => {
       name: "Easy to use UI and fun Projectttt",
     },
   ]
+  const links = [
+    {
+      href: "https://x.com/damnGruz",
+      logo: <FaXTwitter />,
+    },
+    {
+      href: "https://github.com/rinkitadhana",
+      logo: <LuGithub />,
+    },
+    {
+      href: "https://www.linkedin.com/in/rinkitadhana/",
+      logo: <FiLinkedin />,
+    },
+    {
+      href: "https://leetcode.com/u/therinkit/",
+      logo: <SiLeetcode />,
+    },
+    {
+      href: "https://www.instagram.com/rnkktt/",
+      logo: <FaInstagram />,
+    },
+  ]
 
   return (
     <section className="md:px-0 px-2">
       <div className=" flex justify-between items-center">
         <Back path="/" />
-        <a
-          target="_blank"
-          href="https://github.com/rinkitadhana/Anecdot-Blog-Application"
-          className=" border-2 rounded-lg border-black py-1 px-2 text-black flex gap-1 items-center font-medium cursor-pointer  transition-all hover:bg-black hover:text-white "
-        >
-          <FiGithub className=" size-5" />
-          Github
-        </a>
       </div>
-      <section className="flex flex-col gap-4 my-6 border border-zinc-200 p-4 ">
+      <section className="flex flex-col gap-4 my-6 ">
         <div className=" flex flex-col gap-4 items-center">
           <img
             className=" md:size-[220px] rounded"
@@ -67,17 +87,20 @@ const About = () => {
             alt="anecdot."
           />
 
-          <h1 className="flex flex-row gap-1  md:text-3xl text-2xl font-semibold">
+          <h1 className="flex flex-row gap-1   md:text-3xl text-2xl font-semibold">
             Anecdot. - Blog <span className="hidden md:block">Application</span>
             <span className="md:hidden">App</span>
           </h1>
+          <a
+            target="_blank"
+            href="https://github.com/rinkitadhana/Anecdot-Blog-Application"
+            className=" border-2 w-fit rounded-lg border-black py-1 px-2 text-black flex gap-1 items-center font-medium cursor-pointer  transition-all hover:bg-black hover:text-white "
+          >
+            <FiGithub className=" size-5" />
+            Github
+          </a>
 
-          <p className=" text-center">
-            A full stack blogging website built using the MERN stack, offering a
-            variety of features to enhance user experience. The website allows
-            users to manage their profiles, and engage through comments and
-            likes. It also includes user authentication and real-time updates.
-          </p>
+          <p className=" text-center ">{description}</p>
         </div>
         <div className=" flex flex-col gap-2 text-zinc-900">
           <h1 className=" font-semibold text-xl w-fit">Tech Stack</h1>
@@ -126,16 +149,38 @@ const About = () => {
             )}
           </div>
         </div>
-        <div className=" flex flex-col gap-2">
+        {/* <div className=" flex flex-col gap-2">
           <h1 className=" font-semibold text-xl w-fit">Features</h1>
-          <ul className="list-disc list-inside">
+          <ul className=" space-y-1 list-disc list-inside">
             {feat.map((feat) => (
-              <li key={feat.name}>{feat.name}</li>
+              <li className=" gap-y-0" key={feat.name}>
+                {feat.name}
+              </li>
             ))}
           </ul>
-        </div>
-        <div>
+        </div> */}
+        <div className=" flex flex-col gap-2">
           <h1 className=" font-semibold text-xl w-fit">Author</h1>
+          <div className=" flex items-center gap-2 px-2">
+            <img
+              className=" md:w-[8%] w-[25%] rounded-full"
+              src="/img/profile.jpg"
+            />
+            <div className=" flex flex-col gap-0.5">
+              <h1 className=" text-2xl font-semibold">Rinkit Adhana</h1>
+              <div className=" flex gap-2 text-2xl">
+                {links.map((link) => (
+                  <a
+                    className=" hover:text-zinc-500 hover:scale-125 transition-all"
+                    key={link.href}
+                    href={link.href}
+                  >
+                    {link.logo}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </section>
