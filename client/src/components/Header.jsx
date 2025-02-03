51330c2ab574298643d48e0aeb073f3f4ffbeba3
Rinkit Adhana <therinkit@gmail.com>
2025-02-03 17:54:36 +0530

    header enhanced ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -64,7 +64,7 @@ const Header = () => {
 
   return (
     <section className=" fixed top-0 w-full bg-mainWhite/50 dark:bg-mainBlack/50 backdrop-blur-md z-50 select-none font-Inter  ">
-      <div className="flex  justify-between items-center border-b border-primary dark:border-primaryDark   py-2.5 md:px-8 px-4  ">
+      <div className="flex  justify-between items-center border-b border-primary dark:border-primaryDark   md:py-2.5 py-3.5 md:px-8 px-4  ">
         <div className=" flex gap-10 items-center">
           <Link to="/" className=" text-2xl font-bold ">
             Anecdot.
