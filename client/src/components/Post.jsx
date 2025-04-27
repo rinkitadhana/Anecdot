c778be5d790b6262296116e4f80e897f9e185b94
Rinkit Adhana <therinkit@gmail.com>
2025-04-28 01:19:29 +0530

    post page updated

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,6 +1,7 @@
 import { format } from "date-fns"
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import { Calendar, User } from "lucide-react"
 
 const Post = ({ _id, title, summary, cover, createdAt, author }) => {
   const [newTitle, setNewTitle] = useState("")
@@ -20,7 +21,7 @@ const Post = ({ _id, title, summary, cover, createdAt, author }) => {
 
   return (
     <Link to={`/post/${_id}`}>
-      <div className="flex md:flex-row flex-col md:gap-4 gap-2 p-2 hover:bg-primary dark:hover:bg-primaryDark  cursor-pointer rounded-md transition-all">
+      <div className="flex md:flex-row flex-col md:gap-4 gap-2 p-2 border border-zinc-200 dark:border-zinc-700 hover:bg-zinc-100 dark:hover:bg-zinc-800  cursor-pointer rounded-md transition-all">
         <div className=" basis-2/5 ">
           <img
             className="image"
@@ -34,13 +35,17 @@ const Post = ({ _id, title, summary, cover, createdAt, author }) => {
 
         <div className="basis-3/5 flex flex-col gap-0.5">
           <h1 className="text-2xl font-bold ">{newTitle}</h1>
-          <div className="flex gap-3 items-center">
-            <p className=" font-semibold opacity-80  ">
+          <div className="flex gap-4 items-center">
+            <p className=" font-medium opacity-80 flex gap-1 items-center">
+              <User size={16} />
               {author?.fullName || "Anonymous"}
             </p>
-            <time className=" opacity-70 text-sm font-medium">
-              {format(new Date(createdAt), "dd MMM, yyy")}
-            </time>
+            <div className="flex gap-1  opacity-70 items-center">
+              <Calendar size={16} />
+              <time className=" text-sm font-medium">
+                {format(new Date(createdAt), "dd MMM, yyy")}
+              </time>
+            </div>
           </div>
           <p className=" opacity-80">{newSummary}</p>
         </div>
